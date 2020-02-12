cc70f5783769c78eb8c19c3ed03c57664e94f046
Jimmy <born1031.jimmy@gmail.com>
2020-02-12 23:59:52 +0800

    Create controllers.

---
+++ controllers/bookController.js
@@ -0,0 +1,41 @@
+var Book = require('../models/book');
+
+// Display List of all books.
+exports.book_list = function(req, res, next){
+	res.send('NOT IMPLEMENTED: Book list.');
+};
+
+// Display detail page for a specific book.
+exports.book_detail = function(req, res, next){
+	res.send('NOT IMPLEMENTED: Book detail.');
+};
+
+// Display book create form on GET.
+exports.book_create_get = function(req, res, next){
+	res.send('NOT IMPLEMENTED: Book create GET.');
+};
+
+// Handle book create form on POST.
+exports.book_create_post = function(req, res, next){
+	res.send('NOT IMPLEMENTED: Book create POST.');
+};
+
+// Display book delete form on GET.
+exports.book_delete_get = function(req, res, next){
+	res.send('NOT IMPLEMENTED: Book delete GET.');
+};
+
+// Handle book delete form on POST.
+exports.book_delete_post = function(req, res, next){
+	res.send('NOT IMPLEMENTED: Book delete POST.');
+};
+
+// Display book update form on GET.
+exports.book_update_get = function(req, res, next){
+	res.send('NOT IMPLEMENTED: Book update GET.');
+};
+
+// Handle book update form on POST.
+exports.book_update_post = function(req, res, next){
+	res.send('NOT IMPLEMENTED: Book update POST.');
+};
