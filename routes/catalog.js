aa686285416eab29f39b9862d410e5fd89f5196c
Jimmy <born1031.jimmy@gmail.com>
2020-02-13 21:39:04 +0800

    Update catalog.js

--- routes/catalog.js
+++ routes/catalog.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 // Require controller moduls.
-var book_controller = require('../controllers/bookController');
-var author_controller = require('../controllers/authorController');
-var genre_controller = require('../controllers/genreController');
-var bookinstance_controller = require('../controllers/bookinstanceController');
+var book_controller = require("../controllers/bookController");
+var author_controller = require("../controllers/authorController");
+var genre_controller = require("../controllers/genreController");
+var bookinstance_controller = require("../controllers/bookinstanceController"ß);
 
 // // GET catalog home page.
 // router.get('/', function(req, res, next){
