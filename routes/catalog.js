e0fb69677037f352fdb82d16259b7da0161f2bf7
Jimmy <born1031.jimmy@gmail.com>
2020-02-18 00:00:12 +0800

    Update catalog.js
    fix those router.post

--- routes/catalog.js
+++ routes/catalog.js
@@ -18,19 +18,19 @@ router.get('/', book_controller.index);
 router.get('/book/create', book_controller.book_create_get);
 
 // POST request for creating Book.
-router.get('/book/create', book_controller.book_create_post);
+router.post('/book/create', book_controller.book_create_post);
 
 // GET request to delete Book.
 router.get('/book/:id/delete', book_controller.book_delete_get);
 
 // POST request to delete Book.
-router.get('/book/:id/delete', book_controller.book_delete_post);
+router.post('/book/:id/delete', book_controller.book_delete_post);
 
 // GET request to update Book.
 router.get('/book/:id/update', book_controller.book_update_get);
 
 // POST request to update Book.
-router.get('/book/:id/update', book_controller.book_update_post);
+router.post('/book/:id/update', book_controller.book_update_post);
 
 // GET request for one Book.
 router.get('/book/:id', book_controller.book_detail);
