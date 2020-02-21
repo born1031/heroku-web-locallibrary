0e301404cae458187f01930975d2a29f16f1783f
Jimmy <born1031.jimmy@gmail.com>
2020-02-22 00:09:51 +0800

    Update bookController.js

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -236,7 +236,7 @@ exports.book_update_get = function(req, res, next){
 			var err = new Error('Book not found.');
 			err.status = 404;
 			return next(err);
-		}
+		};
 
 		// Success.
 		// Mark our seleted genres as checked.
@@ -244,9 +244,9 @@ exports.book_update_get = function(req, res, next){
 			for(var book_g_iter = 0; book_g_iter < results.book.genre.length; book_g_iter++){
 				if(results.genres[all_g_iter]._id.toString() == results.book.genre[book_g_iter]._id.toString()){
 					results.genres[all_g_iter].checked = 'true';
-				}
-			}
-		}
+				};
+			};
+		};
 
 		res.render('book_form', {title: 'Update Book', authors: results.authors, genres: results.genres, book: results.book});
 	});
